c346b620b78182878dea7ec27706e474d8c24852
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 01:10:28 +0530

    UPDATED: Employee per Month Salary

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,25 +1,38 @@
-#! /bin/bash -x
+#! /bin/bash 
+
+declare -a SalaryRecord
 
 #CONSTANTS
+MAX_WORKING_DAYS=20;
 WAGE_PER_HOUR=20;
 FULL_TIME=8;
 HALF_TIME=4;
+sum=0;
+counter=0;
 
-emp=$((RANDOM%3));
-	case $emp in
-					0)
-						 dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
-						 echo "PRESENT for FULL TIME";
-   					 echo "Wage Today: "$dailyWage;;
+for(( i=0 ; i<=$MAX_WORKING_DAYS ; i++ ))
+do
+	emp=$((RANDOM%3));
+				case $emp in
+								0)
+						 			dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
+						 			SalaryRecord[((counter++))]=$dailyWage;;
 
-					1)
-						dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
-						echo "PRESENT for HALF TIME";
-						echo "Wage Today: "$dailyWage;;
+								1)
+									dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
+									SalaryRecord[((counter++))]=$dailyWage;;
 
-					2)
-						 echo "ABSENT! Sorry NO WAGE.";;
+								2)
+						 			dailyWage=0;
+									SalaryRecord[((counter++))]=$dailyWage;;
 
-					*)
-						echo "Wrong Input!";
-	esac
+								*)
+									echo "Wrong Input!";
+				esac
+done
+echo "Wages per Day: "${SalaryRecord[@]};
+for(( i=0 ; i<=${#SalaryRecord[@]} ; i++ ))
+do
+	sum=$((sum+SalaryRecord[i]))
+done
+	echo "Salary: "$sum;
