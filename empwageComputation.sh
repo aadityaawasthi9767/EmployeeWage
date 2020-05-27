e096cc3bc2faa0371dd04802c556967fcc887680
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 00:02:20 +0530

    UPDATED: Employee Wage Switch Case

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -6,17 +6,20 @@ FULL_TIME=8;
 HALF_TIME=4;
 
 emp=$((RANDOM%3));
-if [[ $emp -eq 0 ]]
-then
-	dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
-	echo "PRESENT for FULL TIME";
-	echo "Wage Today: "$dailyWage;
+	case $emp in
+					0)
+						 dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
+						 echo "PRESENT for FULL TIME";
+   					 echo "Wage Today: "$dailyWage;;
 
-elif [[ $emp -eq 1 ]]
-then
-	dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
-	echo "PRESENT for HALF TIME";
-	echo "Wage Today: "$dailyWage;
-else
-	echo "ABSENT! Sorry NO WAGE.";
-fi
+					1)
+						dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
+						echo "PRESENT for HALF TIME";
+						echo "Wage Today: "$dailyWage;;
+
+					2)
+						 echo "ABSENT! Sorry NO WAGE.";;
+
+					*)
+						echo "Wrong Input!";
+	esac
