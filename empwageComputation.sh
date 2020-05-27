ab114b46a0c5f79e997720aff753b1a161682b16
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-27 23:49:18 +0530

    UPDATED: Employee Half Time

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -2,15 +2,21 @@
 
 #CONSTANTS
 WAGE_PER_HOUR=20;
-FULL_DAY=6;
+FULL_TIME=8;
+HALF_TIME=4;
 
-
-emp=$((RANDOM%2));
+emp=$((RANDOM%3));
 if [[ $emp -eq 0 ]]
 then
-	dailyWage=$(( WAGE_PER_HOUR * FULL_DAY ))
-	echo "Present";
+	dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
+	echo "PRESENT for FULL TIME";
+	echo "Wage Today: "$dailyWage;
+
+elif [[ $emp -eq 1 ]]
+then
+	dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
+	echo "PRESENT for HALF TIME";
 	echo "Wage Today: "$dailyWage;
 else
-	echo "Absent";
+	echo "ABSENT! Sorry NO WAGE.";
 fi
