da7fc1132666044aba4927188127bc290581f50a
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-27 23:36:19 +0530

    UPDATED: Employee Daily Wage

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,8 +1,16 @@
 #! /bin/bash -x
+
+#CONSTANTS
+WAGE_PER_HOUR=20;
+FULL_DAY=6;
+
+
 emp=$((RANDOM%2));
 if [[ $emp -eq 0 ]]
 then
+	dailyWage=$(( WAGE_PER_HOUR * FULL_DAY ))
 	echo "Present";
+	echo "Wage Today: "$dailyWage;
 else
 	echo "Absent";
 fi
