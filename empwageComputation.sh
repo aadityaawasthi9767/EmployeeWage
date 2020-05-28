dfd39b0c85b7f96cf630afb9b4a00d3708ac3fa1
Aaditya Awasthi <aadityaawasyhi97672gmail.com>
2020-05-28 14:39:24 +0530

    UPDATED: Employee Wage Function

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -1,56 +1,32 @@
-#! /bin/bash -x
+#!/bin/bash -x
 
-declare -a SalaryRecord
-declare -a EmployeeHoursArray
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HRS=20
+MAX_HRS_IN_MONTH=4
+NUM_WORKING_DAYS=20
 
-#CONSTANTS
-MAX_WORKING_DAYS=20;
-WAGE_PER_HOUR=20;
-FULL_TIME=8;
-HALF_TIME=4;
-MAX_WORKING_HOURS=100;
-
-#VARIABLES
-totalWorkingHours=0;
-totalNumberofDays=1;
-sum=0;
-counter=0;
-counter2=0
+totalEmpHrs=0
+totalWorkingDays=0
 
 
-while [[ $totalNumberofDays -le $MAX_WORKING_DAYS && $totalWorkingHours -le $MAX_WORKING_HOURS ]]
+function getWorkHrs()
+{
+         case $empCheck in
+        $IS_FULL_TIME)empHrs=8;;
+        $IS_PARK_TIME)empHrs=4;;
+        *)empHrs=0;;
+        esac
+        echo $empHrs
+ }
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	((totalNumberofDays++))
-
-	emp=$((RANDOM%3));
-				case $emp in
-								0)
-									totalWorkingHours=$(( totalWorkingHours + FULL_TIME ))
-						 			dailyWage=$(( WAGE_PER_HOUR * FULL_TIME ));
-						 			SalaryRecord[ ((counter++)) ]=$dailyWage;
-									EmployeeHoursArray[((counter2++))]=$totalWorkingHours;;
-
-								1)
-									totalWorkingHours=$(( totalWorkingHours + HALF_TIME ))
-									dailyWage=$(( WAGE_PER_HOUR * HALF_TIME ));
-									EmployeeHoursArray[((counter2++))]=$totalWorkingHours;
-									SalaryRecord[((counter++))]=$dailyWage;;
-
-								2)
-									totalWorkingHours=$(( totalWorkingHours + 0 ))
-						 			dailyWage=0;
-									SalaryRecord[((counter++))]=$dailyWage;
-									EmployeeHoursArray[((counter2++))]=$totalWorkingHours;;
-
-
-								*)
-									echo "Wrong Input!";
-				esac
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+        empHrs="$( getWorkHrs $empCheck )"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-echo "Wages per Day: "${#SalaryRecord[@]};
-for(( i=0 ; i<=${#SalaryRecord[@]} ; i++ ))
-do
-	sum=$(( sum + SalaryRecord[i] ))
-done
-	echo "Salary: "$sum;
-	echo "Salary Records"${#SalaryRecord[@]}
+
+        salary=$(( $totalEmpHrs * $EMP_RATE_PER_HRS ))
+        
